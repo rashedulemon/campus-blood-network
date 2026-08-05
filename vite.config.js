49df622d2d099b15171acee0a6de39b78d02a197
Md. Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2026-08-06 01:00:46 +0600

    vite setup

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/campus-blood-network/', // Required for GitHub Pages deployment
+  base: '/', // Required for GitHub Pages deployment
   build: {
     target: 'es2015', // Ensures older mobile browsers can run the JavaScript
   },
